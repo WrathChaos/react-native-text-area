6c693cf632173284a46d343af43f870483b2fe61
FreakyCoder <kurayogun@gmail.com>
2019-07-15 18:02:36 +0300

    Example is updated

--- example/App.js
+++ example/App.js
@@ -1,27 +1,11 @@
 import React, { Fragment } from "react";
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar
-} from "react-native";
+import { SafeAreaView } from "react-native";
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions
-} from "react-native/Libraries/NewAppScreen";
-
-import TextArea from "./lib/src/TextArea";
+import TextArea from "react-native-text-area";
 
 const App = () => {
   return (
     <Fragment>
-      <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <TextArea
           maxCharLimit={50}
@@ -33,44 +17,5 @@ const App = () => {
     </Fragment>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter
-  },
-  engine: {
-    position: "absolute",
-    right: 0
-  },
-  body: {
-    backgroundColor: Colors.white
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: "600",
-    color: Colors.black
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: "400",
-    color: Colors.dark
-  },
-  highlight: {
-    fontWeight: "700"
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: "600",
-    padding: 4,
-    paddingRight: 12,
-    textAlign: "right"
-  }
-});
 
 export default App;
